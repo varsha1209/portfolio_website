a122975b91fd60edbde781e3973ba89ba447ccc9
varsha dahare <varsha.dahare@ziroh.com>
2023-11-07 22:25:03 +0530

    Added github , linkdin and resume link

--- src/app/images.js
+++ src/app/images.js
@@ -4,6 +4,10 @@ import Screen1 from "../../public/screen1-image.jpeg";
 import Screen2 from "../../public/screen2-image.jpeg";
 import Screen3 from "../../public/screen3-image.jpeg";
 import Phoenix from "../../public/phoenix-image.jpg";
+import Linkdin from "../../public/linkdin-icon.svg";
+import Github from "../../public/github-icon.svg";
 
 
-export { Sparrow, DeveloperImage , Screen1 ,Screen2 , Screen3 , Phoenix}
+
+
+export { Sparrow, DeveloperImage , Screen1 ,Screen2 , Screen3 , Phoenix , Linkdin , Github}
